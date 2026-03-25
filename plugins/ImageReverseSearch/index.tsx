fe81f56e1b8497659587a9eb095f59d965071d17
thegoldenknight2 <garyspammer1@gmail.com>
2026-03-25 22:48:35 +0200

    Update index.tsx

--- plugins/ImageReverseSearch/index.tsx
+++ plugins/ImageReverseSearch/index.tsx
@@ -3,19 +3,10 @@ import { before, after } from "@vendetta/patcher";
 import { getAssetIDByName } from "@vendetta/ui/assets";
 import { Forms } from "@vendetta/ui/components";
 import { findInReactTree } from "@vendetta/utils";
-import { ReactNative, stylesheet } from "@vendetta/metro/common";
-import { semanticColors } from "@vendetta/ui";
+import { ReactNative } from "@vendetta/metro/common";
 
 const LazyActionSheet = findByProps("openLazy", "hideActionSheet");
-const ActionSheetRow = findByProps("ActionSheetRow")?.ActionSheetRow ?? Forms.FormRow;
-
-const styles = stylesheet.createThemedStyleSheet({
-    icon: {
-        width: 24,
-        height: 24,
-        tintColor: semanticColors.INTERACTIVE_NORMAL
-    }
-});
+const { FormIcon } = Forms;
 
 let patches: (() => void)[] = [];
 
@@ -38,36 +29,68 @@ export default {
                 const unpatch = after("default", instance, (_, res) => {
                     setTimeout(unpatch, 0);
 
-                    const buttons = findInReactTree(res, (x: any) => x?.[0]?.type?.name === "ActionSheetRow");
-                    if (!buttons) return;
-
-                    if (buttons.some((b: any) => b?.props?.label === "Reverse Search Image")) return;
-
-                    const icon = getAssetIDByName("SearchIcon");
-
-                    buttons.push(
-                        <ActionSheetRow
-                            label="Reverse Search Image"
-                            icon={
-                                <ActionSheetRow.Icon
-                                    source={icon}
-                                    IconComponent={() => (
-                                        <ReactNative.Image
-                                            resizeMode="contain"
-                                            style={styles.icon}
-                                            source={icon}
-                                        />
-                                    )}
-                                />
-                            }
-                            onPress={() => {
-                                LazyActionSheet.hideActionSheet();
-                                ReactNative.Linking.openURL(
-                                    `https://lens.google.com/uploadbyurl?url=${encodeURIComponent(imageUrl)}`
-                                );
-                            }}
-                        />
+                    const actionSheetContainer = findInReactTree(
+                        res,
+                        (x: any) => Array.isArray(x) && x[0]?.type?.name === "ActionSheetRowGroup"
                     );
+                    const buttons = findInReactTree(
+                        res,
+                        (x: any) => x?.[0]?.type?.name === "ButtonRow"
+                    );
+
+                    if (buttons) {
+                        if (buttons.some((b: any) => b?.props?.label === "Reverse Search Image")) return;
+                        buttons.push(
+                            <Forms.FormRow
+                                label="Reverse Search Image"
+                                leading={
+                                    <FormIcon
+                                        style={{ opacity: 1 }}
+                                        source={getAssetIDByName("ic_search")}
+                                    />
+                                }
+                                onPress={() => {
+                                    LazyActionSheet.hideActionSheet();
+                                    ReactNative.Linking.openURL(
+                                        `https://lens.google.com/uploadbyurl?url=${encodeURIComponent(imageUrl)}`
+                                    );
+                                }}
+                            />
+                        );
+                    } else if (actionSheetContainer && actionSheetContainer[1]) {
+                        const middleGroup = actionSheetContainer[1];
+                        const ActionSheetRow = middleGroup.props.children[0].type;
+                        const existingIcon = middleGroup.props.children[0].props.icon;
+
+                        if (middleGroup.props.children.some((b: any) => b?.props?.label === "Reverse Search Image")) return;
+
+                        middleGroup.props.children.push(
+                            <ActionSheetRow
+                                label="Reverse Search Image"
+                                icon={{
+                                    $$typeof: existingIcon.$$typeof,
+                                    type: existingIcon.type,
+                                    key: null,
+                                    ref: null,
+                                    props: {
+                                        IconComponent: () => (
+                                            <FormIcon
+                                                style={{ opacity: 1 }}
+                                                source={getAssetIDByName("ic_search")}
+                                            />
+                                        ),
+                                    },
+                                }}
+                                onPress={() => {
+                                    LazyActionSheet.hideActionSheet();
+                                    ReactNative.Linking.openURL(
+                                        `https://lens.google.com/uploadbyurl?url=${encodeURIComponent(imageUrl)}`
+                                    );
+                                }}
+                                key="reverse-search"
+                            />
+                        );
+                    }
                 });
             });
         }));
